0a45dd494b16a75c63951e3a6bb3c52f7910b336
wilstenholme <woraman.lo@gmail.com>
2015-12-10 11:30:17 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 43d2e46d785986a3e42dc5583f164034e40ce83c, reversing
    changes made to f3badc0c34b722670a3e1ad3f31f5e8ee2e59467.

--- src/testProject/Main.java
+++ src/testProject/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package testProject;
 
 public class Main {
@@ -24,29 +23,3 @@ public class Main {
         System.out.println(myTaco.LogTime());
 	}
 }
-=======
-package testProject;
-
-public class Main {
-	public static void main(String[] args){
-		Taco myTaco = new Taco();
-		Taco yourTaco = new Taco();
-		Taco thisisaTaco = new Taco();
-		
-		myTaco.AddTopping("lettuce");
-		myTaco.AddTopping("meat");
-		myTaco.AddTopping("cheese");
-		myTaco.AddTopping("hot sauce");
-		thisisaTaco.AddTopping("cheese");
-
-		yourTaco = myTaco.Clone();
-		yourTaco.RemoveTopping("hot sauce");
-
-		System.out.println(myTaco.ShowTacoCost());
-		System.out.println(yourTaco.ShowTacoCost());
-        System.out.println(thisisaTaco.ShowTacoCost());
-		
-        System.out.println(myTaco.LogTime());
-	}
-}
->>>>>>> origin/master
